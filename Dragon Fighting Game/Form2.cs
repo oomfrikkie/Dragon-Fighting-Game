cc2d39167e32555e6b6515e4aa9411a9e338c639
derjen <derjen.frick@gmail.com>
2024-05-21 09:04:37 +0200

    added 2 more methods

--- Dragon Fighting Game/Form2.cs
+++ Dragon Fighting Game/Form2.cs
@@ -14,46 +14,47 @@ namespace Dragon_Fighting_Game
     //player block button
     public partial class Form2 : Form
     {
-       //declaring the crucial variables
+        //declaring the crucial variables
         string[] p1Data2;
         string[] p2Data2;
         int[] p1Values2;
         int[] p2Values2;
-       
+
         bool player1Blocking = false;
         bool player2Blocking = false;
 
-        int currentPlayer = 1;
-        int RoundCounter = 1;
+        int currentPlayer = 0;
+        int RoundCounter = 0;
         string player1Action;
         string player2Action;
         Random random = new Random();
 
+            bool player1Special = false;
+            bool player2Special = false;
 
         public Form2(string[] p1Data, string[] p2Data, int[] p1Values, int[] p2Values) //bringing arrays from form1
-        { 
+        {
             p1Data2 = p1Data;
             p2Data2 = p2Data;
             p1Values2 = p1Values;
             p2Values2 = p2Values;
-           
             InitializeComponent();
-            
-            
+
+
         }
 
         //loading form 2
         private void Form2_Load(object sender, EventArgs e)
         {
-          TakeInitiative();
-           
+            TakeInitiative();
+
         }
 
 
 
         public int RandomRoll()
         {
-           
+
             return random.Next(1, 7);
         }
         public void TakeInitiative() //determines whos going first for the round
@@ -67,25 +68,25 @@ namespace Dragon_Fighting_Game
                 player2Roll = RandomRoll(); // this uses the RandomRoll() to determine who goes first
 
             }
-            if ( player1Roll > player2Roll)
+            if (player1Roll > player2Roll)
             {
                 gbx5.Text = ($"{p1Data2[1]},  the {p1Data2[2]} turn ");
                 label1.Text = ($"HP: {p1Values2[0]}");
                 lblDragon1Hp.Text = ("HP" + p1Values2[0]);
                 currentPlayer = 1;
             }
-            else if ( player1Roll < player2Roll)
+            else if (player1Roll < player2Roll)
             {
                 gbx5.Text = ($"{p2Data2[1]},  the {p2Data2[2]} turn ");
                 label1.Text = ($"HP: {p2Values2[0]}");
-                lblDragon1Hp.Text = ("HP" + p2Values2[0]) ;
+                lblDragon1Hp.Text = ("HP" + p2Values2[0]);
                 currentPlayer = 2;
             }
-           
-            
-          
-       
-            
+
+
+
+
+
         }
         private void lblDragonHp2_Click(object sender, EventArgs e)
         {
@@ -95,17 +96,12 @@ namespace Dragon_Fighting_Game
 
         {
 
-            if (RoundCounter == 3)
-            {
-                RoundCounter = 1;
-                currentPlayer = 1;
-                RandomRoll();
-            }
 
 
+            TakeInitiative();
             if (currentPlayer == 1)
             {
-               
+
                 gbx5.Text = ($"{p1Data2[1]},  the {p1Data2[2]} turn ");
                 label1.Text = ($"HP: {p1Values2[0]}");
                 gbx6.Text = ("Opponent" + p2Data2[1]);
@@ -115,17 +111,16 @@ namespace Dragon_Fighting_Game
             }
             else if (currentPlayer == 2)
             {
-                
+
                 gbx5.Text = ($"{p2Data2[1]},  the {p2Data2[2]} turn ");
                 label1.Text = ($"HP: {p1Values2[0]}");
                 gbx6.Text = ("Opponent" + p1Data2[1]);
                 lblDragon1Hp.Text = ($"HP: {p2Values2[0]}");
-               
+
 
             }
-            TakeInitiative();
         }
-       
+
         private void Attack()
         {
             int damage;
@@ -142,34 +137,34 @@ namespace Dragon_Fighting_Game
                 else
                 {
                     p2Values2[0] -= p1Values2[1];
-                    
-                    
+
+
                     rtbBattleLog.Text += "\n" + p1Data2[1] + " attacked for " + p1Values2[1];
                 }
-                
+
             }
             else if (currentPlayer == 2)
             {
 
                 if (player1Blocking == true)//if the opponent blocks
                 {
-                    
+
                     player1Blocking = false;
                     damage = p2Values2[1] - p1Values2[2];// working out damage subtracted by the block
                     p2Values2[0] -= Math.Abs(damage);
-                    rtbBattleLog.Text += "\n" + p2Data2[1] + " attacked but"+ p1Data2[1] + " blocked " + damage + "  damage was done ";
+                    rtbBattleLog.Text += "\n" + p2Data2[1] + " attacked but" + p1Data2[1] + " blocked " + damage + "  damage was done ";
 
                 }
                 else
                 {
                     p1Values2[0] -= p2Values2[1];
-                   
+
                     player1Blocking = false;
                 }
 
 
             }
-            
+
 
 
         }
@@ -185,16 +180,17 @@ namespace Dragon_Fighting_Game
             {
                 player2Blocking = true;
                 player1Blocking = false;
-               
+
 
             }
-           
+
 
 
         }
         private void SAttack() // special attack funtionality
         {
             int damage;
+            
             if (currentPlayer == 1)
             {
 
@@ -212,6 +208,8 @@ namespace Dragon_Fighting_Game
 
                     rtbBattleLog.Text += "\n" + p1Data2[1] + "used his special attack for " + p1Values2[3];
                 }
+                player1Special = true;
+                
 
             }
             else if (currentPlayer == 2)
@@ -232,9 +230,10 @@ namespace Dragon_Fighting_Game
                     player1Blocking = false;
                     rtbBattleLog.Text += "\n" + p2Data2[1] + "used his special attack for " + p2Values2[3];
 
-
+                    player2Special = true;
 
                 }
+                Rest();
 
             }
         }
@@ -242,21 +241,21 @@ namespace Dragon_Fighting_Game
         private void btnAtk_Click_1(object sender, EventArgs e) // code for atk button
         {
             Attack();
-          
+
             CheckDefeated();
             SwitchTurn();
-            
+
             RoundCounter++;
         }
 
-       
+
         private void btnBlock_Click(object sender, EventArgs e) // code for the block button
         {
             Block();
-            
+
             CheckDefeated();
-            SwitchTurn(); 
-            
+            SwitchTurn();
+
             RoundCounter++;
         }
 
@@ -264,16 +263,16 @@ namespace Dragon_Fighting_Game
         {
 
             SAttack();
-            
+
             CheckDefeated();
             SwitchTurn();
-            
+
             RoundCounter++;
         }
 
-      
 
-      
+
+
         private void CheckDefeated()//here is where the victor is decided
         {
             if (p1Values2[0] <= 0)
@@ -289,6 +288,33 @@ namespace Dragon_Fighting_Game
         private void gbx5_Enter(object sender, EventArgs e)
         {
 
+        }
+        private void RoundCount()
+        {
+            if (RoundCounter == 2)
+            {
+                TakeInitiative();
+                RandomRoll();
+                RoundCounter = 0;
+            }
+
+        }
+        private void Rest()
+        {
+            if (player1Special == true)
+            {
+                if (currentPlayer == 1)
+                {
+                    btnRest.Show();
+                }
+            }
+            else if (player2Special== true)
+            {
+                if (currentPlayer == 2)
+                {
+                    btnRest.Show();
+                }
+            }
         }
     }
 }
